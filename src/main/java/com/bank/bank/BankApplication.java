64f0ccea1194e42e76f8a2cde9ddcd6378d66b35
Victor Perdomo <victorgabperdomo@gmail.com>
2025-02-03 20:28:59 -0500

    Create Main class and implemented CommandLineRunner on BankApplication

--- src/main/java/com/bank/bank/BankApplication.java
+++ src/main/java/com/bank/bank/BankApplication.java
@@ -1,13 +1,22 @@
 package com.bank.bank;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bank.bank.main.Main;
+
 @SpringBootApplication
-public class BankApplication {
+public class BankApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.Menu();
+		
+	}
 }
---
+++ src/main/java/com/bank/bank/main/Main.java
@@ -0,0 +1,10 @@
+package com.bank.bank.main;
+
+public class Main {
+
+
+
+    public void Menu() {
+        
+    }
+}
