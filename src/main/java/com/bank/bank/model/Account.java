59b928be585541515871a9f42bbd1f6be81f2033
Victor Perdomo <victorgabperdomo@gmail.com>
2025-02-07 22:07:29 -0500

    Update Account class

--- src/main/java/com/bank/bank/model/Account.java
+++ src/main/java/com/bank/bank/model/Account.java
@@ -2,7 +2,6 @@ package com.bank.bank.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Random;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,6 +19,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 
 public class Account {
 
@@ -31,26 +31,10 @@ public class Account {
     private String password;    // must be at least 8 characters
     private double balance;
     private String accountNumber;  // must be 16 digits and unique
-    private String currency;    
+    private String currency;  
     private String phoneNumber;    // must be 10 digits and unique
     private LocalDateTime createdAt; 
     private boolean isActive;
     private LocalDate dateOfBirth;
 
-
-
-    public Account() {
-        this.accountNumber = generateAccountNumber();
-    }
-
-    private String generateAccountNumber() {
-        // generate a random 16 digit number
-        StringBuilder sb = new StringBuilder();
-        Random random = new Random();
-
-        for (int i = 0; i < 16; i++) {
-            sb.append(random.nextInt(10));
-        }
-    return sb.toString();
-    }
 }
